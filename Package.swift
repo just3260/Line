d3031eba519aee1b9c2a5fd5cfd1259cfdc58e6b
”just3260” <just3260@gmail.com>
2018-06-09 03:26:26 +0800

    [add] MySQL

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ let package = Package(
     name: "Line",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "2.4.0"),
-        .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "2.4.0")),
+        .package(url: "https://github.com/vapor-community/mysql-provider", from: "2.0.0"),
         .package(url: "https://github.com/happiness9721/line-bot-sdk-swift.git", .upToNextMajor(from: "2.0.2")),
         .package(url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Fluent", "LineBot", "Jobs"]),
+        .target(name: "App", dependencies: ["Vapor", "MySQLProvider", "LineBot", "Jobs"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
